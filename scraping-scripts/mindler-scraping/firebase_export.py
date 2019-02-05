36ace066a5cf8322cc8822389c2e6bec62db729e
xxator <parth.agarwal131@gmail.com>
2019-02-05 17:24:05 +0530

    work on firebase data upload

---
+++ scraping-scripts/mindler-scraping/firebase_export.py
@@ -0,0 +1,87 @@
+#from firebase import firebase  
+import csv
+import pickle
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyA1QuFJ-oeqLp0Q0akmBPVy9YUY84cxsoc",
+  "authDomain": "counselling-bot-10fda.firebaseapp.com",
+  "databaseURL": "https://counselling-bot-10fda.firebaseio.com",
+  "storageBucket": "counselling-bot-10fda.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+class Career:
+    def __init__(self,url):
+        url2=url
+        page = urllib.request.urlopen(url2)
+        soup = BeautifulSoup(page, 'html.parser')
+        div = soup.find(class_="career-details-section")
+        name = div.find('h2')
+        self.name = name.text
+        print(self.name)
+        self.summary = getsummary(url)
+        #print('\n','summary','\n',self.summary)
+        self.professions = getproffesional(url)
+        #print('\n','Proffesions','\n',self.professions)
+        self.career_path = getcareerpath(url)
+        #print('\n','career-path','\n',self.career_path)
+        self.important_facts = get_important_facts(url)
+        #print('\n','important-facts','\n',self.important_facts)
+        self.leading_colleges = getleadinginstitute(url)
+        #print('\n','leading_colleges','\n',self.leading_colleges)
+        self.institutions_abroad = getinstitutionsabroad(url)
+        #print('\n','institutions abroad','\n',self.institutions_abroad)
+        self.entrance_exams = get_entrance_exams(url)
+        #print('\n','entrance_exams')
+        #print(self.entrance_exams)
+        self.work_description = get_work_description(url)
+        #print('\n','work description','\n',self.work_description)
+        self.pros,self.cons = get_pros_cons(url)
+        #print("pros")
+        #print(self.pros)
+        #print("cons")
+        #print(self.cons)
+        #print('\n\n')
+        
+class Field:
+    def __init__(self,url):
+        url2=url
+        page = urllib.request.urlopen(url2)
+        soup = BeautifulSoup(page, 'html.parser')
+        div = soup.find(id='career-bucket-page',class_="career-library-new career-details-page career-last-page")
+        name = div.find('h4')
+        self.name = name.text
+        career = []
+        div = soup.find('div',id="library-data-bucket",class_="cl-row search-topics last-page")
+        temp = div.find_all('a')
+        urls = []
+        for i in temp:
+            career.append(Career(i.get('href')))
+
+objects = []
+with (open("CareerLibrary", "rb")) as openfile:
+	while True:
+		try:
+			objects.append(pickle.load(openfile))
+		except:
+			break
+# firebase = firebase.FirebaseApplication('https://counselling-bot-10fda.firebaseio.com/', None)
+
+
+filehandler = open("Field_list.txt",'r')
+inp = filehandler.readlines()
+db = firebase.database()
+
+
+# Pass the user's idToken to the push method
+#results = db.child("users").push(data, user['idToken'])
+
+for i in inp:
+	data = {
+			'name' : i
+	}
+	db.child("Fieldlist").push(data)
+
+
