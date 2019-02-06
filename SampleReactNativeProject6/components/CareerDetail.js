579bd466f6019c449b342bd1e4702e86aa5dff07
arindam-modak <arindam.modak03@gmail.com>
2019-02-07 02:34:17 +0530

    career detail page error persists

--- SampleReactNativeProject6/components/CareerDetail.js
+++ SampleReactNativeProject6/components/CareerDetail.js
@@ -13,41 +13,27 @@ class CareerDetail extends Component {
 
   constructor(props) {
     super(props);
-    this.ref = firebase.firestore().collection('AllCareers').where("name", "==", JSON.parse(this.props.navigation.getParam('CareerName')));
-    this.unsubscribe = null;
     this.state = {
-      
       isLoading: true,
-      fields: []
+      board: {},
+      key: ''
     };
   }
 
-  onCollectionUpdate = async (querySnapshot) => {
-    const fields = [];
-    await querySnapshot.forEach(async (doc) => {
-      fields.push({
-        key: doc.id,
-        name: doc.name,
-        career_path: doc.career_path,
-        cons: doc.cons,
-        entrance_exams: doc.entrance_exams,
-        important_facts: doc.important_facts,
-        institutions_abroad: doc.institutions_abroad,
-        leading_colleges: doc.leading_colleges,
-        professions: doc.professions,
-        summary: doc.summary,
-        superCareer: doc.superCareer,
-        work_description: doc.work_description
-      });
-    });
-    this.setState({
-      fields,
-      isLoading: false,
-    });
-  }
-
   componentDidMount() {
-    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+    const { navigation } = this.props;
+    const ref = firebase.firestore().collection('AllCareers').where("name", "==", JSON.parse(navigation.getParam('CareerName')));
+    ref.get().then((doc) => {
+      if (doc.exists) {
+        this.setState({
+          board: doc.data(),
+          key: doc.id,
+          isLoading: false
+        });
+      } else {
+        console.log("No such document!");
+      }
+    });
   }
 
   render() {
@@ -61,16 +47,14 @@ class CareerDetail extends Component {
     return (
       <ScrollView style={styles.container}>
         <View>
-          this.state.fields.map((item, i) => (
             <Text>
               {'Name'}{'\n'}
-              {item.name}{'\n'}
+              {this.state.board.name}{'\n'}
             </Text>
             <Text>
               {'\n'}{'Summary'}{'\n'}
-              {item.summary}
+              {this.state.board.summary}
             </Text>
-          ))
         </View>
       </ScrollView>
     );
